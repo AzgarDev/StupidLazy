21d69cc68b08d8efd82c2c0e38e8fb951710a04c
AzgarDev <71645505+AzgarDev@users.noreply.github.com>
2020-10-09 22:35:43 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+echo "Installing..."
+sleep 1
+apt-get install figlet
+apt-get install python-pip
+pip3 install colorama
+clear
+chmod +x stupidlazy.py
+chmod +x NMAPer.py
+chmod +x page2.py
+chmod +x UPDATE.py
+clear
+echo "Done"
